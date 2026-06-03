3fc725e24dcbb6b358d22c05ca0f5f254f2e2e8a
Maxim <maxmatto13@gmail.com>
2026-06-03 16:04:47 +0200

    MainLayouts

---
+++ src/layouts/mainLayout.tsx
@@ -0,0 +1,23 @@
+import { PropsWithChildren } from "@kitajs/html"
+
+type InputProps = PropsWithChildren<{}>
+
+export default ({ children }: InputProps) => (
+
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>User Management</title>
+        
+        
+                <link rel="stylesheet" href="/public/style.css" />
+
+      </head>
+      <body>
+        <main class="container">
+          {children}
+        </main>
+      </body>
+    </html>
+  );
