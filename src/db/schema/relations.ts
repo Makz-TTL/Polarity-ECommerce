29992b2e2c392ebf2f400536d31e0a5eeb095793
Riccardo Lannunziata <riccardo.lannunziata@studenti.iispascal.it>
2026-06-03 16:27:45 +0200

    Working on relations.txt

--- src/db/schema/relations.ts
+++ src/db/schema/relations.ts
@@ -3,14 +3,45 @@ import { products } from "./products"
 import { reviews } from "./reviews"
 import { productUsefulness } from "./productUsefulness"
 import {orders} from "./orders"
-//import { defineRelations } from "drizzle-orm/relations";
+import { relations } from "drizzle-orm/relations";
 
 
 
-// export default defineRelations({ users, products, reviews, productUsefulness, orders }, (r) => ({
+// Relazioni per gli UTENTI
+export const usersRelations = relations(users, ({ many }) => ({
+  orders: many(orders),
+  reviews: many(reviews),
+  usefulnessVotes: many(productUsefulness),
+  soldProducts: many(orders)
+}));
+
+// Relazioni per i PRODOTTI
+export const productsRelations = relations(products, ({ many }) => ({
+  reviews: many(reviews),
+}));
+
+// Relazioni per le RECENSIONI
+export const reviewsRelations = relations(reviews, ({ one, many }) => ({
+  author: one(users, {
+    fields: [reviews.userId],
+    references: [users.id],
+  }),
+  product: one(products, {
+    fields: [reviews.productId],
+    references: [products.id],
+  }),
+  usefulness: many(productUsefulness),
+}));
 
 
-
-
-
-// }))
+// Relazioni per gli ORDINI
+export const ordersRelations = relations(orders, ({ one }) => ({
+  buyer: one(users, {
+    fields: [orders.userId],
+    references: [users.id],
+  }),
+  product: one(products, {
+    fields: [orders.productId],
+    references: [products.id],
+  }),
+}));
