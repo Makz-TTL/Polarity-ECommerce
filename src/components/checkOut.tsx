52913dd34cc5953548fd0a97457e7afeff371b59
Riccardo Lannunziata <riccardo.lannunziata@studenti.iispascal.it>
2026-06-03 16:18:24 +0200

    Created components folder and checkout page, not completed

---
+++ src/components/checkOut.tsx
@@ -0,0 +1,18 @@
+import MainLayout from "../layouts/mainLayout";
+
+export default function CheckOut() {
+
+    // return (
+
+
+    // //    <MainLayout>
+    // //     <h1>Checkout Page</h1>
+    // //     <p>Hi {user.name}! this is a recap of your order.</p>
+    // //   </MainLayout> 
+
+
+    // )
+
+
+
+}
--- src/db/schema/relations.ts
+++ src/db/schema/relations.ts
@@ -3,14 +3,14 @@ import { products } from "./products"
 import { reviews } from "./reviews"
 import { productUsefulness } from "./productUsefulness"
 import {orders} from "./orders"
-import { defineRelations } from "drizzle-orm/relations";
+//import { defineRelations } from "drizzle-orm/relations";
 
 
 
-export default defineRelations({ users, products, reviews, productUsefulness, orders }, (r) => ({
+// export default defineRelations({ users, products, reviews, productUsefulness, orders }, (r) => ({
 
 
 
 
 
-}))
+// }))
